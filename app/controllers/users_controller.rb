661e31f85da3c29be69d917f694f492a897d6852
omori09 <g21933oc@gm.tsuda.ac.jp>
2024-03-19 18:12:24 +0900

    わわあわ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,21 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id]) 
   end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to user_path(current_user.id)
+    else
+      render :edit
+    end
+  end
+
+  def after_sign_up_path_for(resource)
+    redirect_to user_path(current_user.id)
+  end
   
+  #アカウント編集後のリダイレクト先
+  def after_update_path_for(resource)
+    redirect_to user_path(current_user.id)
+  end
 end
